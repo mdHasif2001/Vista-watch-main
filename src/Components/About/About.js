e763658aa726651c853939e7f1b6e7eb082f3f52
mdHasif2001 <iamhasib727@gmail.com>
2022-04-05 22:20:46 +0600

    about section creatd

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            <h2>This is about section</h2>
+            <h2 className='about'>About the Product</h2>
+            <div className='about-watch'>
+                <div className='about-txt'>
+                    <p><b>Largest Display</b></p>
+                    <p>The new Retina display on Apple Watch Series 7 has nearly 20 percent more screen area than Series 6.</p>
+                </div>
+                <div className='about-txt'>
+                    <p><b>Improved Durability</b></p>
+                    <p>Most crack-resistant front crystal. Dust resistant. And swimproof.footnote1</p>
+                </div>
+                <div className='about-txt'>
+                    <p><b>Fast Charging</b></p>
+                    <p>Same all-day battery lifefootnote2 with up to 33 percent faster charging than Series 6.footnote3</p>
+                </div>
+            </div>
         </div>
     );
 };
